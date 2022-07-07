b54d96448353a574e5ad0ee379b11139cc5b4463
markkong30 <markkong30@gmail.com>
2022-07-07 23:31:37 +0100

    sanity setup

---
+++ backend/schemas/user.js
@@ -0,0 +1,17 @@
+export default {
+	name: "user",
+	ttitle: "User",
+	type: "document",
+	fields: [
+		{
+			name: "userName",
+			title: "User Name",
+			type: "string",
+		},
+		{
+			name: "image",
+			title: "Image",
+			type: "file",
+		},
+	],
+};
