29a5c0f84e7396dcbb5d22f7d382733a0f45d660
markkong30 <markkong30@gmail.com>
2022-07-23 12:05:26 +0100

    sidebar setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,21 @@
 import type { NextPage } from 'next'
+import axios from 'axios'
 
 
 const Home: NextPage = () => {
   return (
     <div className='text-bold text-pink-500'>
-      Dream
+      
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const response = await axios.get(`http://localhost:3000/api/post`)
+
+  return {
+    props: {}
+  }
 }
 
 export default Home
